5834e5e5102617a118321bc65a748fcb9775ece8
Anton <anton9121@gmail.com>
2016-11-17 10:20:51 +0500

    interpreter as class

---
+++ interpreter.js
@@ -0,0 +1,315 @@
+/**
+ * Created by anton on 15.11.16.
+ */
+var Interpreter = function (details) {
+  "use strict";
+  var _this = this;
+  _this.details = details || {};
+  _this.scope = {};
+  _this.initScope();
+};
+
+Interpreter.prototype.initScope = function () {
+  var _this = this;
+  var details = _this.details;
+  var customScope = details.scope || {};
+  var scope = _this.scope;
+
+  Object.keys(customScope).forEach(function (key) {
+    scope[key] = customScope[key];
+  });
+};
+
+Interpreter.prototype.getVariable = function (scope, variable) {
+  var _this = this;
+  if (typeof variable !== 'string') {
+    return {value: scope[variable], context: scope};
+  }
+
+  var items = variable.split('.');
+
+  var context = scope;
+  var value = scope[items.shift()];
+  while (items.length) {
+    context = value;
+    value = context[items.shift()];
+  }
+
+  return {value: value, context: context};
+};
+
+Interpreter.prototype.getVariableFunction = function (localScope, variable) {
+  var _this = this;
+  if (typeof variable !== 'object') {
+    return _this.getVariable(localScope, variable);
+  } else {
+    return {context: _this.scope, value: _this.runCommand(localScope, variable)};
+  }
+};
+
+Interpreter.prototype.getContext = function (context, localScope, forceContext) {
+  var _this = this;
+  if (forceContext) {
+    return _this.getVariableValue(localScope, forceContext);
+  } else {
+    return context || _this.scope;
+  }
+};
+
+Interpreter.prototype.buildArgs =function (scope, args) {
+  var _this = this;
+  args = args || [];
+  var fnArgs = new Array(args.length);
+  args.forEach(function (variable, i) {
+    fnArgs[i] = _this.getVariableValue(scope, variable);
+  });
+  return fnArgs;
+};
+
+Interpreter.prototype.getVariableValue = function (scope, variable) {
+  var _this = this;
+  if (typeof variable !== 'object') {
+    return _this.getVariable(scope, variable).value;
+  } else {
+    return _this.runCommand(scope, variable);
+  }
+};
+
+Interpreter.prototype.getVariableScope = function (scope, _variable) {
+  var _this = this;
+  var variable = _variable;
+
+  var items = null;
+  if (typeof variable === 'string') {
+    items = variable.split('.');
+    variable = items.shift();
+  }
+
+  var _scope;
+  while (!scope.hasOwnProperty(variable)) {
+    _scope = scope.prototype;
+    if (_scope === undefined) {
+      break;
+    }
+    scope = _scope;
+  }
+
+  if (items) {
+    while (items.length) {
+      scope = scope[variable];
+      variable = items.shift();
+    }
+  }
+
+  return scope;
+};
+
+Interpreter.prototype.getLocalScope = function (scope, context, args, callArgs) {
+  callArgs = callArgs || [];
+  args = args || [];
+
+  var localScope = Object.create(scope);
+  localScope.prototype = scope;
+  localScope['arguments'] = callArgs;
+  localScope.this = context;
+
+  args.forEach(function (varible, index) {
+    localScope[varible] = callArgs[index];
+  });
+
+  return localScope;
+};
+
+Interpreter.prototype.commands = {
+  var: function (_this, scope, command) {
+    scope[command.name] = _this.getVariableValue(scope, command.value);
+  },
+  call: function (_this, scope, command) {
+    var details = _this.getVariableFunction(scope, command.value);
+    var context = _this.getContext(details.context, scope, command.context);
+    var fnArgs = _this.buildArgs(scope, command.args);
+    var fn = details.value;
+    if (typeof fn !== 'function') {
+      throw new Error('Call ' + JSON.stringify(command.value) + ' is not a function!');
+    }
+    return fn.apply(context, fnArgs);
+  },
+  apply: function (_this, scope, command) {
+    var details = _this.getVariableFunction(scope, command.value);
+    var context = _this.getContext(details.context, scope, command.context);
+    var fnArgs = _this.getVariableValue(scope, command.args);
+    var fn = details.value;
+    if (typeof fn !== 'function') {
+      throw new Error('Apply ' + JSON.stringify(command.value) + ' is not a function!');
+    }
+    return fn.apply(context, fnArgs);
+  },
+  new: function (_this, scope, command) {
+    var details = _this.getVariableFunction(scope, command.value);
+    var args = _this.buildArgs(scope, command.args);
+    var fn = details.value;
+    if (!args.length) {
+      return new fn();
+    } else
+    if (args.length === 1) {
+      return new fn(args[0]);
+    } else
+    if (args.length === 2) {
+      return new fn(args[0], args[1]);
+    } else
+    if (args.length === 3) {
+      return new fn(args[0], args[1], args[3]);
+    }
+  },
+  '=': function (_this, scope, command) {
+    var value = _this.getVariableValue(scope, command.value);
+    var variable = command.var;
+    var varScope = _this.getVariableScope(scope, variable);
+    varScope[variable] = value;
+    return value;
+  },
+  '+=': function (_this, scope, command) {
+    var value = _this.getVariableValue(scope, command.value);
+    var variable = command.var;
+    var varScope = _this.getVariableScope(scope, variable);
+    varScope[variable] += value;
+    return value;
+  },
+  '-=': function (_this, scope, command) {
+    var value = _this.getVariableValue(scope, command.value);
+    var variable = command.var;
+    var varScope = _this.getVariableScope(scope, variable);
+    varScope[variable] -= value;
+    return value;
+  },
+  return: function (_this, scope, command) {
+    var value = _this.getVariableValue(scope, command.value);
+    scope.return = true;
+    return value;
+  },
+  raw: function (_this, scope, command) {
+    return command.data;
+  },
+  function: function (_this, scope, command) {
+    return function () {
+      var localScope = _this.getLocalScope(scope, this, command.args, [].slice.call(arguments));
+      return _this.execScript(localScope, command.value);
+    };
+  },
+  statement: function (_this, scope, command) {
+    return _this.execScript(scope, command.value);
+  },
+  if: function (_this, scope, command) {
+    var result = _this.getVariableValue(scope, command.condition);
+    if (command.not) {
+      result = !result;
+    }
+    if (result) {
+      return command.then && _this.execScript(scope, command.then);
+    } else {
+      return command.else && _this.execScript(scope, command.else);
+    }
+  },
+  throw: function (_this, scope, command) {
+    throw _this.getVariableValue(scope, command.value);
+  },
+  try: function (_this, scope, command) {
+    var result;
+    try {
+      result = _this.execScript(scope, command.value);
+    } catch (err) {
+      if (command.catch) {
+        var localScope = _this.getLocalScope(scope, this, command.args, [err]);
+        result = _this.execScript(localScope, command.catch);
+      }
+    }
+    return result;
+  },
+  '+': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    return args.reduce(function(sum, current) {
+      return sum + current;
+    }, 0);
+  },
+  '-': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    return args.reduce(function(value, current) {
+      return value - current;
+    }, args.shift());
+  },
+  '*': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    return args.reduce(function(value, current) {
+      return value * current;
+    }, args.shift());
+  },
+  '/': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    return args.reduce(function(value, current) {
+      return value / current;
+    }, args.shift());
+  },
+  '==': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    var first = args.shift();
+    return args.every(function (value) {
+      return first == value;
+    });
+  },
+  '===': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    var first = args.shift();
+    return args.every(function (value) {
+      return first === value;
+    });
+  },
+  '&&': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    return args.every(function(value) {
+      return !!value;
+    });
+  },
+  '||': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    return args.some(function(value) {
+      return !!value;
+    });
+  },
+  '>': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    return args[0] > args[1];
+  },
+  '<': function (_this, scope, command) {
+    var args = _this.buildArgs(scope, command.args);
+    return args[0] < args[1];
+  }
+};
+
+Interpreter.prototype.runCommand = function (scope, command) {
+  var _this = this;
+  var type = command.type;
+  return _this.commands[type](_this, scope, command);
+};
+
+Interpreter.prototype.execScript = function (localScope, script) {
+  var _this = this;
+  var index = 0;
+  var len = script.length;
+  var next = function () {
+    var command = script[index++];
+    var result = _this.runCommand(localScope, command);
+    if (len === index || localScope.return === true) {
+      return result;
+    } else {
+      return next();
+    }
+  };
+  return next();
+};
+
+Interpreter.prototype.runScript = function (script) {
+  var _this = this;
+  return _this.execScript(_this.scope, script);
+};
+
+module.exports = Interpreter;
--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 /**
- * Created by anton on 15.11.16.
+ * Created by Anton on 17.11.2016.
  */
-var scope = {};
-scope.log = function () {
-  console.log.apply(console, arguments);
-};
-scope.console = console;
-scope.RegExp = RegExp;
+var Interpreter = require('./interpreter');
 
 var myScript = [
   {type: 'var', name: 'myFunction', value: {type: 'function', args: ['var1'], value: [
@@ -14,7 +9,7 @@ var myScript = [
     {type: '=', var: 'test', value: {type: 'raw', data: 'test 2'}},
     {type: 'if', condition: {type: 'raw', data: true},
       then: [
-        {type: 'call', value: 'log', args: [{type: 'raw', data: 'if 1 true'}]},
+        {type: 'call', value: 'console.log', args: [{type: 'raw', data: 'if 1 true'}]},
         {type: '=', var: 'myArr', value: {type: 'raw', data: [1]}}
       ],
       else: [
@@ -23,7 +18,7 @@ var myScript = [
     },
     {type: 'if', not: true, condition: {type: 'raw', data: true},
       then: [
-          {type: 'return', value: {type: 'raw', data: 'abort'}}
+        {type: 'return', value: {type: 'raw', data: 'abort'}}
       ]
     },
     {type: 'if', condition: {type: 'raw', data: true},
@@ -37,11 +32,11 @@ var myScript = [
         ]}
       ]
     },
-    {type: 'call', value: 'log', args: ['var1']},
+    {type: 'call', value: 'console.log', args: ['var1']},
     {type: 'return', value: 'test'}
   ]}},
   {type: 'var', name: 'myFunctionWithContext', value: {type: 'function', args: ['var1'], value: [
-    {type: 'call', value: 'log', args: ['this', 'var1']}
+    {type: 'call', value: 'console.log', args: ['this', 'var1']}
   ]}},
   {type: 'var', name: 'myArr', value: {type: 'raw', data: []}},
   {type: 'var', name: 'myObj', value: {type: 'raw', data: {}}},
@@ -51,22 +46,22 @@ var myScript = [
   {type: 'var', name: 'myRegExp', value: {type: 'new', value: 'RegExp', args: [{type: 'raw', data: 'test'}]}},
   {type: 'var', name: 'myVarA', value: {type: 'raw', data: 1}},
   {type: 'var', name: 'myVarB', value: {type: 'raw', data: 2}},
-  {type: 'call', value: 'log', args: [
-      {type: 'call', value: 'myFunction', args: [{type: 'raw', data: 'arg1'}]}
+  {type: 'call', value: 'console.log', args: [
+    {type: 'call', value: 'myFunction', args: [{type: 'raw', data: 'arg1'}]}
   ]},
   {type: 'call', value: 'myFunctionWithContext', context: {type: 'raw', data: {a:1, b: 2}}, args: [
-      {type: 'raw', data: 'arg1value'}
+    {type: 'raw', data: 'arg1value'}
   ]},
-  {type: 'call', value: 'log', args: ['myArr']},
-  {type: 'call', value: 'log', args: ['myObj', 'myObj.test']},
-  {type: 'call', value: 'log', args: ['myUndefined']},
-  {type: 'call', value: 'log', args: ['myNull']},
-  {type: 'call', value: 'log', args: [
+  {type: 'call', value: 'console.log', args: ['myArr']},
+  {type: 'call', value: 'console.log', args: ['myObj', 'myObj.test']},
+  {type: 'call', value: 'console.log', args: ['myUndefined']},
+  {type: 'call', value: 'console.log', args: ['myNull']},
+  {type: 'call', value: 'console.log', args: [
     'myRegExp',
     {type: 'call', value: 'myRegExp.test', args: [{type: 'raw', data: 'test'}]}
   ]},
-  {type: 'call', value: 'log', args: ['myVarA']},
-  {type: 'call', value: 'log', args: ['myVarB']},
+  {type: 'call', value: 'console.log', args: ['myVarA']},
+  {type: 'call', value: 'console.log', args: ['myVarB']},
   {type: 'call', value: 'console.log', args: ['myVarB']},
   {type: 'try', value: [
     {type: 'call', value: 'console.log', args: [{type: 'raw', data: 'in try'}]},
@@ -75,7 +70,7 @@ var myScript = [
   ], args: ['err'], catch: [
     {type: 'call', value: 'console.log', args: ['err']}
   ]},
-  {type: 'call', value: 'log', args: [
+  {type: 'call', value: 'console.log', args: [
     {type: '+', args: ['myVarA', 'myVarB']},
     {type: '-', args: ['myVarA', 'myVarB']},
     {type: '*', args: ['myVarA', 'myVarB']},
@@ -89,288 +84,15 @@ var myScript = [
   ]}
 ];
 
-var getVariable = function (scope, variable) {
-  if (typeof variable !== 'string') {
-    return {value: scope[variable], context: scope};
-  }
-
-  var items = variable.split('.');
-
-  var context = scope;
-  var value = scope[items.shift()];
-  while (items.length) {
-    context = value;
-    value = context[items.shift()];
-  }
-
-  return {value: value, context: context};
-};
-
-var buildArgs = function (scope, args) {
-  args = args || [];
-  var fnArgs = new Array(args.length);
-  args.forEach(function (variable, i) {
-    fnArgs[i] = getVariableValue(scope, variable);
-  });
-  return fnArgs;
-};
-
-var getVariableValue = function (scope, variable) {
-  if (typeof variable !== 'object') {
-    return getVariable(scope, variable).value;
-  } else {
-    return runCommand(scope, variable);
-  }
-};
-
-var getVariableFunction = function (localScope, variable) {
-  if (typeof variable !== 'object') {
-    return getVariable(localScope, variable);
-  } else {
-    return {context: scope, value: runCommand(localScope, variable)};
-  }
-};
-
-var getLocalScope = function (scope, context, args, callArgs) {
-  callArgs = callArgs || [];
-  args = args || [];
-
-  var localScope = Object.create(scope);
-  localScope.prototype = scope;
-  localScope.arguments = callArgs;
-  localScope.this = context;
-
-  args.forEach(function (varible, index) {
-    localScope[varible] = callArgs[index];
-  });
-
-  return localScope;
-};
-
-var getVariableScope = function (scope, _variable) {
-  var variable = _variable;
-
-  var items = null;
-  if (typeof variable === 'string') {
-      items = variable.split('.');
-      variable = items.shift();
-  }
-
-  var _scope;
-  while (!scope.hasOwnProperty(variable)) {
-    _scope = scope.prototype;
-    if (_scope === undefined) {
-      break;
-    }
-    scope = _scope;
-  }
-
-  if (items) {
-    while (items.length) {
-        scope = scope[variable];
-        variable = items.shift();
-    }
-  }
-
-  return scope;
-};
-
-var getContext = function (context, localScope, forceContext) {
-  if (forceContext) {
-    return getVariableValue(localScope, forceContext);
-  } else {
-    return context || scope;
-  }
-};
-
-var commands = {
-  var: function (scope, command) {
-    scope[command.name] = getVariableValue(scope, command.value);
-  },
-  call: function (scope, command) {
-    var details = getVariableFunction(scope, command.value);
-    var context = getContext(details.context, scope, command.context);
-    var fnArgs = buildArgs(scope, command.args);
-    var fn = details.value;
-    if (typeof fn !== 'function') {
-      throw new Error('Call ' + JSON.stringify(command.value) + ' is not a function!');
-    }
-    return fn.apply(context, fnArgs);
-  },
-  apply: function (scope, command) {
-    var details = getVariableFunction(scope, command.value);
-    var context = getContext(details.context, scope, command.context);
-    var fnArgs = getVariableValue(scope, command.args);
-    var fn = details.value;
-    if (typeof fn !== 'function') {
-      throw new Error('Apply ' + JSON.stringify(command.value) + ' is not a function!');
-    }
-    return fn.apply(context, fnArgs);
-  },
-  new: function (scope, command) {
-      var details = getVariableFunction(scope, command.value);
-      var args = buildArgs(scope, command.args);
-      var fn = details.value;
-      if (!args.length) {
-          return new fn();
-      } else
-      if (args.length === 1) {
-          return new fn(args[0]);
-      } else
-      if (args.length === 2) {
-          return new fn(args[0], args[1]);
-      } else
-      if (args.length === 3) {
-          return new fn(args[0], args[1], args[3]);
-      }
-  },
-  '=': function (scope, command) {
-    var value = getVariableValue(scope, command.value);
-    var variable = command.var;
-    var varScope = getVariableScope(scope, variable);
-    varScope[variable] = value;
-    return value;
-  },
-  '+=': function (scope, command) {
-    var value = getVariableValue(scope, command.value);
-    var variable = command.var;
-    var varScope = getVariableScope(scope, variable);
-    varScope[variable] += value;
-    return value;
-  },
-  '-=': function (scope, command) {
-    var value = getVariableValue(scope, command.value);
-    var variable = command.var;
-    var varScope = getVariableScope(scope, variable);
-    varScope[variable] -= value;
-    return value;
-  },
-  return: function (scope, command) {
-    var value = getVariableValue(scope, command.value);
-    scope.return = true;
-    return value;
-  },
-  raw: function (scope, command) {
-    return command.data;
-  },
-  function: function (scope, command) {
-    return function () {
-      var localScope = getLocalScope(scope, this, command.args, [].slice.call(arguments));
-      return execScript(localScope, command.value);
-    };
-  },
-  statement: function (scope, command) {
-    return execScript(scope, command.value);
-  },
-  if: function (scope, command) {
-    var result = getVariableValue(scope, command.condition);
-    if (command.not) {
-      result = !result;
-    }
-    if (result) {
-      return command.then && execScript(scope, command.then);
-    } else {
-      return command.else && execScript(scope, command.else);
-    }
-  },
-  throw: function (scope, command) {
-    throw getVariableValue(scope, command.value);
-  },
-  try: function (scope, command) {
-    var result;
-    try {
-      result = execScript(scope, command.value);
-    } catch (err) {
-      if (command.catch) {
-        var localScope = getLocalScope(scope, this, command.args, [err]);
-        result = execScript(localScope, command.catch);
-      }
-    }
-    return result;
-  },
-  '+': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    return args.reduce(function(sum, current) {
-      return sum + current;
-    }, 0);
-  },
-  '-': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    return args.reduce(function(value, current) {
-      return value - current;
-    }, args.shift());
-  },
-  '*': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    return args.reduce(function(value, current) {
-      return value * current;
-    }, args.shift());
-  },
-  '/': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    return args.reduce(function(value, current) {
-      return value / current;
-    }, args.shift());
-  },
-  '==': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    var first = args.shift();
-    return args.every(function (value) {
-      return first == value;
-    });
-  },
-  '===': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    var first = args.shift();
-    return args.every(function (value) {
-      return first === value;
-    });
-  },
-  '&&': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    return args.every(function(value) {
-      return !!value;
-    });
-  },
-  '||': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    return args.some(function(value) {
-      return !!value;
-    });
-  },
-  '>': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    return args[0] > args[1];
-  },
-  '<': function (scope, command) {
-    var args = buildArgs(scope, command.args);
-    return args[0] < args[1];
-  }
-};
-
-var runCommand = function (scope, command) {
-  var type = command.type;
-  return commands[type](scope, command);
-};
-
-var execScript = function (localScope, script) {
-  var index = 0;
-  var len = script.length;
-  var next = function () {
-    var command = script[index++];
-    var result = runCommand(localScope, command);
-    if (len === index || localScope.return === true) {
-      return result;
-    } else {
-      return next();
-    }
-  };
-  return next();
-};
-
 (function () {
+  var interpreter = new Interpreter({
+    scope: {
+      console: console,
+      RegExp: RegExp
+    }
+  });
   return Promise.resolve().then(function () {
-    return execScript(scope, myScript);
+    return interpreter.runScript(myScript);
   }).then(function () {
     console.log('result', arguments);
   }, function (err) {
